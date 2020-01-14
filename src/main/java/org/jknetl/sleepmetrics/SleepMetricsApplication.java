5097912930cfd4d6c86909d3df949bc3e300f622
Jakub Knetl <knetl.j@gmail.com>
2020-01-14 13:55:51 +0100

    Initialize Spring project

---
+++ src/main/java/org/jknetl/sleepmetrics/SleepMetricsApplication.java
@@ -0,0 +1,13 @@
+package org.jknetl.sleepmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jknetl/sleepmetrics/SleepMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jknetl.sleepmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
