0be9563f293258225bee4b1589ad052fad7bea9e
Jakub Knetl <knetl.j@gmail.com>
2020-02-12 10:29:18 +0100

    Add bootstrap template with thymeleaf standard layout system

--- src/main/java/org/jknetl/sleepmetrics/config/SecurityConfig.java
+++ src/main/java/org/jknetl/sleepmetrics/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package org.jknetl.sleepmetrics.config;
 
 import net.bytebuddy.build.Plugin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -30,10 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .passwordEncoder(passwordEncoder());
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/register")
+                .antMatchers("/", "/register", "/css/**", "/js/**", "/img/**")
                 .permitAll()
                 .antMatchers("/h2-console/**")
                 .permitAll()
