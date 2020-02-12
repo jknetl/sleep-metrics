d93fadc2df6a6b0f7e0e2008202cfca4f9722ab5
Jakub Knetl <knetl.j@gmail.com>
2020-02-12 11:00:42 +0100

    WIP: introduce Thymeleaf's layout dialect

--- src/main/java/org/jknetl/sleepmetrics/config/WebConfig.java
+++ src/main/java/org/jknetl/sleepmetrics/config/WebConfig.java
@@ -1,9 +1,13 @@
 package org.jknetl.sleepmetrics.config;
 
+import ch.mfrey.thymeleaf.extras.with.WithDialect;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -18,4 +22,23 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
+
+    /*
+     * This bean is used by thymeleaf to resolve templates with thymeleaf's
+     * layout dialect.
+     */
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+    /*
+     * This bean is used by thymeleaf to resolve templates with thymeleaf's
+     * layout dialect.
+     */
+    @Bean
+    public WithDialect withDialect() {
+        return new WithDialect();
+    }
+
 }
